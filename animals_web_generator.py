af9fdf23118c76b2f492cb28fcd9876132593715
NorbertFabian65 <n.fabian@arcor.de>
2026-04-16 17:35:06 +0200

    Code in Funktionen strukturiert und HTML generiert

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,57 +2,63 @@ import json
 
 
 def load_data(file_path):
-    """ Lädt die JSON-Datei """
+    """Lädt die JSON-Daten aus der angegebenen Datei."""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
 
+def serialize_animal(animal_obj):
+    """
+    Verarbeitet ein einzelnes Tier-Objekt und gibt einen HTML-String zurück.
+    Folgt der Struktur aus Schritt 4.
+    """
+    output = '<li class="cards__item">\n'
+
+    # Tiername als Titel
+    if "name" in animal_obj:
+        output += f'  <div class="card__title">{animal_obj["name"]}</div>\n'
+
+    output += '  <p class="card__text">\n'
+
+    characteristics = animal_obj.get("characteristics", {})
+
+    # Details hinzufügen, falls vorhanden
+    if "diet" in characteristics:
+        output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
+
+    if "locations" in animal_obj and animal_obj["locations"]:
+        output += f'      <strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+
+    if "type" in characteristics:
+        output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
+
+    output += '  </p>\n'
+    output += '</li>\n'
+    return output
+
+
 def main():
+    """Hauptprogramm: Steuert den gesamten Prozess."""
     # 1. Daten laden
     animals_data = load_data('animals_data.json')
 
-    # 2. Finales HTML-Design generieren
-    output = ''
+    # 2. HTML-String für alle Tiere generieren
+    animals_html = ""
     for animal in animals_data:
-        # Start der Karte
-        output += '<li class="cards__item">\n'
-
-        # Titel der Karte
-        if "name" in animal:
-            output += f'  <div class="card__title">{animal["name"]}</div>\n'
-
-        # Textbereich der Karte
-        output += '  <p class="card__text">\n'
-
-        characteristics = animal.get("characteristics", {})
-
-        # Ernährung (Diet)
-        if "diet" in characteristics:
-            output += f'      <strong>Diet:</strong> {characteristics["diet"]}<br/>\n'
-
-        # Ort (Location)
-        if "locations" in animal and animal["locations"]:
-            output += f'      <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
-
-        # Typ (Type)
-        if "type" in characteristics:
-            output += f'      <strong>Type:</strong> {characteristics["type"]}<br/>\n'
-
-        output += '  </p>\n'
-        output += '</li>\n'
+        animals_html += serialize_animal(animal)
 
     # 3. Vorlage lesen
     with open("animals_template.html", "r") as f:
         template_content = f.read()
 
-    # 4. Ersetzen
-    new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
+    # 4. Platzhalter ersetzen
+    final_html = template_content.replace("__REPLACE_ANIMALS_INFO__", animals_html)
 
-    # 5. Speichern
+    # 5. Finale Datei speichern
     with open("animals.html", "w") as f:
-        f.write(new_html_content)
+        f.write(final_html)
 
-    print("Glückwunsch! Das finale Design wurde in 'animals.html' gespeichert.")
+    print("Webseite erfolgreich mit strukturierter Funktion generiert!")
 
 
 if __name__ == "__main__":
