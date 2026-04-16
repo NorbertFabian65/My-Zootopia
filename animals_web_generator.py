3de95543511fd4de3b1494b8f0e590bf6859d80a
NorbertFabian65 <n.fabian@arcor.de>
2026-04-16 17:10:53 +0200

    Tierdaten in HTML einfügen

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,36 +11,44 @@ def main():
     # 1. Daten laden
     animals_data = load_data('animals_data.json')
 
-    # 2. String mit den Tierdaten erzeugen (wie in Schritt 1, aber als String)
+    # 2. HTML-String mit Serialisierung erzeugen
     output = ''
     for animal in animals_data:
-        if "name" in animal:
-            output += f"Name: {animal['name']}\n"
+        # Jedes Tier beginnt mit einem Listenelement-Tag
+        output += '<li class="cards__item">\n'
 
+        if "name" in animal:
+            output += f"  <div class=\"card__title\">{animal['name']}</div>\n"
+
+        output += '  <p class="card__text">\n'
+
+        # Details hinzufügen
         characteristics = animal.get("characteristics", {})
         if "diet" in characteristics:
-            output += f"Diet: {characteristics['diet']}\n"
+            output += f"    <strong>Diet:</strong> {characteristics['diet']}<br/>\n"
 
         if "locations" in animal and len(animal["locations"]) > 0:
-            output += f"Location: {animal['locations'][0]}\n"
+            output += f"    <strong>Location:</strong> {animal['locations'][0]}<br/>\n"
 
         if "type" in characteristics:
-            output += f"Type: {characteristics['type']}\n"
+            output += f"    <strong>Type:</strong> {characteristics['type']}<br/>\n"
 
-        output += "\n"  # Leerzeile für den Abstand im Text
+        output += '  </p>\n'
+        # Jedes Tier endet mit dem schließenden Tag
+        output += '</li>\n'
 
-    # 3. Inhalt der Vorlage (Template) lesen
+    # 3. Vorlage lesen
     with open("animals_template.html", "r") as f:
         template_content = f.read()
 
-    # 4. Platzhalter __REPLACE_ANIMALS_INFO__ durch Tierdaten ersetzen
+    # 4. Ersetzen
     new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
-    # 5. Den neuen HTML-Inhalt in die Datei 'animals.html' schreiben
+    # 5. Speichern
     with open("animals.html", "w") as f:
         f.write(new_html_content)
 
-    print("Die Datei animals.html wurde erfolgreich erstellt!")
+    print("Die Datei animals.html wurde mit HTML-Karten erstellt!")
 
 
 if __name__ == "__main__":
